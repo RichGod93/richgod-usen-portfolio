08c123dbb3361c553504fed2f8dce6bf8d09a8f2
RichGod93 <usenrichgod93@gmail.com>
2025-02-11 02:23:07 +0100

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+import { heroui } from "@heroui/react";
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {},
+      colors: {
+        brand: {},
+      },
+    },
+  },
+  plugins: [heroui()],
+};
